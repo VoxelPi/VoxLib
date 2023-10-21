929521b9da6fe2384a6ff39d19f6e1f68e52fbd1
Peter Smek <smek.peter@gmail.com>
2023-10-21 16:18:37 +0200

    Add interpolation functions

--- voxlib-math/src/main/kotlin/net/voxelpi/voxlib/math/Math.kt
+++ voxlib-math/src/main/kotlin/net/voxelpi/voxlib/math/Math.kt
@@ -1,5 +1,9 @@
 package net.voxelpi.voxlib.math
 
+import net.voxelpi.voxlib.math.vector.Vector2D
+import net.voxelpi.voxlib.math.vector.Vector3D
+import kotlin.math.pow
+
 /**
  * Calculates ceil(a/b) as an integer.
  * This function can be used to calculate the number of pages required to display a collection
@@ -7,4 +11,32 @@ package net.voxelpi.voxlib.math
  */
 fun ceilIntDiv(dividend: Int, divisor: Int): Int {
     return dividend / divisor + if (dividend % divisor > 0) 1 else 0 // (num + divisor - 1) / divisor;
+}
+
+/**
+ * Linearly interpolates between the points [a] and [b] by [t].
+ */
+fun interpolateLinear(a: Double, b: Double, t: Double): Double {
+    return a + (b - a) * t
+}
+
+/**
+ * Linearly interpolates between the points [a] and [b] by [t].
+ */
+fun interpolateLinear(a: Vector2D, b: Vector2D, t: Double): Vector2D {
+    return a + (b - a) * t
+}
+
+/**
+ * Linearly interpolates between the points [a] and [b] by [t].
+ */
+fun interpolateLinear(a: Vector3D, b: Vector3D, t: Double): Vector3D {
+    return a + (b - a) * t
+}
+
+/**
+ * Exponentially interpolates between the points [a] and [b] by [t].
+ */
+fun interpolateExponential(a: Double, b: Double, t: Double): Double {
+    return a.pow(1 - t) * b.pow(t)
 }
