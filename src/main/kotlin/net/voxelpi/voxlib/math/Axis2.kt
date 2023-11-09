b1051eb4104e606a0a459aaa36bf4e6682d2550d
Peter Smek <smek.peter@gmail.com>
2023-11-09 23:40:12 +0100

    Added axis enum

---
+++ src/main/kotlin/net/voxelpi/voxlib/math/Axis2.kt
@@ -0,0 +1,12 @@
+package net.voxelpi.voxlib.math
+
+import net.voxelpi.voxlib.math.vector.Vector2I
+import net.voxelpi.voxlib.math.vector.vector2I
+
+public enum class Axis2(
+    public val vector: Vector2I
+) {
+    X(vector2I(1, 0)),
+    Y(vector2I(0, 1)),
+    ;
+}
---
+++ src/main/kotlin/net/voxelpi/voxlib/math/Axis3.kt
@@ -0,0 +1,13 @@
+package net.voxelpi.voxlib.math
+
+import net.voxelpi.voxlib.math.vector.Vector3I
+import net.voxelpi.voxlib.math.vector.vector3I
+
+public enum class Axis3(
+    public val vector: Vector3I
+) {
+    X(vector3I(1, 0, 0)),
+    Y(vector3I(0, 1, 0)),
+    Z(vector3I(0, 0, 1)),
+    ;
+}
