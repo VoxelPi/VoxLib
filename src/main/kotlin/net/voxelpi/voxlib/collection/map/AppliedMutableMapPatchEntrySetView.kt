f5a21ddd3cc3c3057fe081b28388b03c298b2122
Peter Smek <smek.peter@gmail.com>
2026-01-22 00:35:11 +0100

    Implement entry set view

--- src/main/kotlin/net/voxelpi/voxlib/collection/map/AppliedMutableMapPatchEntrySetView.kt
+++ src/main/kotlin/net/voxelpi/voxlib/collection/map/AppliedMutableMapPatchEntrySetView.kt
@@ -1,8 +1,31 @@
 package net.voxelpi.voxlib.collection.map
 
+import net.voxelpi.voxlib.collection.set.AppliedMutableSetPatch
+import net.voxelpi.voxlib.collection.set.AppliedMutableSetPatchIterator
+import net.voxelpi.voxlib.collection.set.MutableSetPatchImpl
+import net.voxelpi.voxlib.collection.set.SetPatch
+
 internal class AppliedMutableMapPatchEntrySetView<K, V>(
     private val appliedPatch: AppliedMutableMapPatchImpl<K, V>,
-) : MutableSet<MutableMap.MutableEntry<K, V>> {
+) : AppliedMutableSetPatch<MutableMap.MutableEntry<K, V>> {
+
+    override val base: Set<MutableMap.MutableEntry<K, V>>
+        get() = appliedPatch.base.keys.map { MutableEntryView(appliedPatch, it) }.toSet()
+
+    override val patch: SetPatch<MutableMap.MutableEntry<K, V>>
+        get() {
+            return MutableSetPatchImpl(
+                appliedPatch.patch.modified.keys.map { MutableEntryView(appliedPatch, it) }.toMutableSet(),
+                appliedPatch.patch.removed.map {
+                    @Suppress("UNCHECKED_CAST")
+                    CachedMutableEntryView(appliedPatch, it, appliedPatch.base[it] as V)
+                }.toMutableSet(),
+            )
+        }
+
+    override fun reduce(): Set<MutableMap.MutableEntry<K, V>> {
+        return appliedPatch.keys.reduce().map { MutableEntryView(appliedPatch, it) }.toSet()
+    }
 
     override val size: Int
         get() = appliedPatch.size
@@ -20,7 +43,7 @@ internal class AppliedMutableMapPatchEntrySetView<K, V>(
     }
 
     override fun iterator(): MutableIterator<MutableMap.MutableEntry<K, V>> {
-        TODO("Not yet implemented")
+        return AppliedMutableSetPatchIterator(this)
     }
 
     override fun add(element: MutableMap.MutableEntry<K, V>): Boolean {
@@ -75,6 +98,45 @@ internal class AppliedMutableMapPatchEntrySetView<K, V>(
     }
 
     override fun clear() {
-        return appliedPatch.clear()
+        appliedPatch.clear()
+    }
+
+    private class MutableEntryView<K, V>(
+        private val appliedPatch: AppliedMutableMapPatchImpl<K, V>,
+        override val key: K,
+    ) : MutableMap.MutableEntry<K, V> {
+
+        override fun setValue(newValue: V): V {
+            @Suppress("UNCHECKED_CAST")
+            return appliedPatch.put(key, value) as V
+        }
+
+        override val value: V
+            get() {
+                @Suppress("UNCHECKED_CAST")
+                return appliedPatch[key] as V
+            }
+    }
+
+    private class CachedMutableEntryView<K, V>(
+        private val appliedPatch: AppliedMutableMapPatchImpl<K, V>,
+        override val key: K,
+        private val previousValue: V,
+    ) : MutableMap.MutableEntry<K, V> {
+
+        override fun setValue(newValue: V): V {
+            @Suppress("UNCHECKED_CAST")
+            return appliedPatch.put(key, value) as V
+        }
+
+        override val value: V
+            get() {
+                if (key !in appliedPatch) {
+                    return previousValue
+                }
+
+                @Suppress("UNCHECKED_CAST")
+                return appliedPatch[key] as V
+            }
     }
 }
